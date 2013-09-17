963a3031d618b7e31e1202006ba448534d913314
knoob <knoob@outlook.com>
2013-09-16 21:40:07 -0500

    Got basic directive test working

--- src/app/SentinelEnvironments/Directive.Spec.js
+++ src/app/SentinelEnvironments/Directive.Spec.js
@@ -1,14 +1,17 @@
 describe('EnvironmentSelector Directive', function () {
-    var $compile, $rootScope, element, scope;
+    var $compile, $rootScope, element
 
     beforeEach(module('sultanApp'));
     beforeEach(inject(function (_$compile_, _$rootScope_) {
+
         $compile = _$compile_;
+
         $rootScope = _$rootScope_;
     }));
-    it('Should display the name of the environment attribute', function () {
-        var element = $compile('<EnvironmentSelector></EnvironmentSelector>')
-        alert(element);
-        expect(element.hasClass('btn').toBe(true));
+    it('Should be testable, callable in a test', function () {
+        var linkingFn = $compile('<button></button>')
+        element = linkingFn($rootScope);
+        alert(element.hasClass('btn'));
+        expect(element.hasClass('btn')).toBe(true);
     });
 })
--- src/app/SentinelEnvironments/EnvironmentSelectorDirective.js
+++ src/app/SentinelEnvironments/EnvironmentSelectorDirective.js
@@ -1,19 +1,35 @@
-sultanApp.directive('EnvironmentSelector', function factory() {
-    alert('hello');
-    var directiveDefintionObject =
-{
-    priority: 0,
-    template: '<div>xxxx</div>', // or // function(tElement, tAttrs) { ... },
-    // or
-    // templateUrl: 'directive.html', // or // function(tElement, tAttrs) { ... },
-    replace: false,
-    transclude: false,
-    restrict: 'E',
-    scope: false,
-    compile: function (elelment, attributes) {
-        element.addClass('btn');
-    },
-    controller: function ($scope, $element, $attrs, $transclude, otherInjectables) { }
-}
-    return directiveDefintionObject;
-})
+sultanApp.directive('button', function factory() {
+    return {
+        restrict: 'E',
+        compile: function (element, attributes) {
+            element.addClass('btn');
+            if (attributes.type == 'submit') {
+                element.addClass('btn-primary');
+            }
+        }
+    }
+});
+
+
+
+//sultanApp.directive('EnvironmentSelector', function factory() {
+//    alert('hello');
+//    var directiveDefintionObject =
+//{
+//    priority: 0,
+//    template: '<div>xxxx</div>', // or // function(tElement, tAttrs) { ... },
+//    // or
+//    // templateUrl: 'directive.html', // or // function(tElement, tAttrs) { ... },
+//    replace: false,
+//    transclude: false,
+//    restrict: 'E',
+//    scope: false,
+//    compile: function (elelment, attributes) {
+//        element.addClass('btn');
+//    },
+//    controller: function ($scope, $element, $attrs, $transclude, otherInjectables) { }
+//}
+//    return directiveDefintionObject;
+//})
+
+
